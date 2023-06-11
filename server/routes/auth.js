eaf40a834df11197e120eac54937e28434267209
Cyber <chanhvokts@gmail.com>
2023-06-12 01:49:07 +0700

    user authentication, routes, jwt token, is admin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-  res.json({data: "Cyber Chanh Vo",});
-});
+// middlewares
+import { requireSignin, isAdmin } from "../middlewares/auth.js";
+//controller
+// import { users } from "../controllers/auth.js";
+import { register, login, secret } from "../controllers/auth.js";
+
+// router.get("/users", users);
+router.post("/register", register);
+router.post("/login", login);
+// testing
+router.get("/secret", requireSignin, isAdmin, secret);
 
 export default router;
